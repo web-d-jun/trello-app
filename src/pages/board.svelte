7a1b7d01aca6f7490b9ee34d1767651d96bdc5e6
junyoung <cjy874545@gamil.com>
2021-09-08 22:31:12 +0900

    update

--- src/pages/board.svelte
+++ src/pages/board.svelte
@@ -6,17 +6,65 @@
   const changeInput = async (event, value) => {
     inputMode = value;
   };
+  const createCard = (event) => {
+    event.preventDefault();
+    if (event.code === "Enter") {
+      console.log(event.target.value);
+    }
+  };
+
+  let headerInputMode = false;
+  const focusOut = (target) => {
+    if (target === "listHeaderName") {
+      headerInputMode = false;
+    }
+  };
+  const focusIn = (e, target) => {
+    if (target === "listHeaderName") {
+      headerInputMode = true;
+      e.target.select();
+    }
+  };
+  const updateName = (e) => {
+    if (!e.shiftKey && e.keyCode === 13) {
+      console.log(e.shiftKey, e.keyCode);
+      e.preventDefault();
+      // console.log(e.blur())
+      e.target.blur();
+      headerInputMode = false;
+    }
+  };
 </script>
 
 <div class="board-main">
   <div class="board-header">header</div>
   <div class="board-body">
-    <div class="add-container" class:add-mode={inputMode}>
+    <div class="list-wrap">
+      <div class="list-content">
+        <div class="list-header">
+          <textarea
+            class="list-header-name"
+            class:input-mode={headerInputMode}
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            on:keydown={($event) => updateName($event)}
+            on:focus={($event) => focusIn($event, "listHeaderName")}
+            on:blur={() => focusOut("listHeaderName")}>header</textarea
+          >
+          <button type="button" class="more-button"><span class="material-icons-outlined"> more_horiz </span></button>
+        </div>
+        <!-- <div class="list-card">list card</div> -->
+        <div class="card-composer-container" />
+      </div>
+    </div>
+    <div class="add-list" class:add-mode={inputMode}>
       <div class="add__contents">
         {#if !inputMode}
           <span class="inner__text" on:click|preventDefault={($event) => changeInput($event, true)}>+ Add another list</span>
         {:else}
-          <input type="text" class="list-name-input" placeholder="Enter list title..." />
+          <input type="text" class="list-name-input" placeholder="Enter list title..." on:keyup|preventDefault={createCard} />
         {/if}
         <div class="flex align-center add-button__wrap" class:visible-mode={inputMode}>
           <button type="button" class="add-button">Add list</button>
@@ -45,7 +93,65 @@
     .board-body {
       border: 1px solid blue;
       height: 100%;
-      .add-container {
+      & .list-wrap {
+        display: inline-block;
+        width: 272px;
+        height: 100%;
+        vertical-align: top;
+        border-radius: 4px;
+
+        .list-content {
+          background-color: #ebecf0;
+          display: flex;
+          flex-direction: column;
+          border-radius: 4px;
+          cursor: pointer;
+          .list-header {
+            width: 100%;
+            min-height: 20px;
+            flex: 0 0 auto;
+            padding: 10px 8px;
+            position: relative;
+            padding-right: 44px;
+            .more-button {
+              position: absolute;
+              top: 50%;
+              right: 8px;
+              transform: translateY(-50%);
+              margin: 0;
+              width: 30px;
+              height: 30px;
+              background-color: #ebecf0;
+              border: 0;
+              outline: 0;
+              .material-icons-outlined {
+                font-size: 1.6rem;
+              }
+            }
+            .list-header-name {
+              resize: none;
+              width: 100%;
+              height: 28px;
+              border: 0;
+              margin: -4px 0;
+              padding: 4px 8px;
+              min-height: 20px;
+              font-size: 1.6rem;
+              background-color: transparent;
+              cursor: pointer;
+              display: block;
+              &.input-mode {
+                box-shadow: inset 0 0 0 2px #144ff7;
+                background-color: #fafbfc;
+                cursor: text;
+              }
+            }
+          }
+        }
+      }
+      .add-list {
+        display: inline-block;
+        vertical-align: top;
         background-color: #00000014;
         padding: 4px;
         border-radius: 4px;
@@ -60,6 +166,11 @@
         }
         &:active {
           background: rgba(#000000, 0.2);
+        }
+        &:not(.add-mode) {
+          &:hover {
+            background: rgba(#000000, 0.2);
+          }
         }
 
         .add__contents {
@@ -77,6 +188,7 @@
             position: relative;
             font-size: 16px;
             box-shadow: inset 0 0 0 2px #144ff7;
+            margin: 0;
           }
           .add-button__wrap {
             margin: 4px 0 0;
